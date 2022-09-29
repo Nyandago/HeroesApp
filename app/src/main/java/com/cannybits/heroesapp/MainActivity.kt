7875e7947916fb40a22dddaa9b7f76e7f48bb999
Nyandago <paulmutator@gmail.com>
2022-09-29 22:25:17 +0300

    MainActivity.kt

--- app/src/main/java/com/cannybits/heroesapp/MainActivity.kt
+++ app/src/main/java/com/cannybits/heroesapp/MainActivity.kt
@@ -10,13 +10,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.cannybits.heroesapp.navigation.SetupNavGraph
 import com.cannybits.heroesapp.ui.theme.HeroesAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HeroesAppTheme {
+                
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
 
             }
         }
