89af301be190c98e7ce132796f1e4f7c02d79264
Nyandago <paulmutator@gmail.com>
2022-09-30 18:41:49 +0300

    Constants.kt

--- app/src/main/java/com/cannybits/heroesapp/util/Constants.kt
+++ app/src/main/java/com/cannybits/heroesapp/util/Constants.kt
@@ -2,6 +2,7 @@ package com.cannybits.heroesapp.util
 
 object Constants{
     const val DETAILS_ARGUMENT_KEY = "heroId"
+    const val CANNYVILLE_DATABASE = "cannyville_database"
     const val HERO_DATABASE_TABLE = "hero_table"
     const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
 }
