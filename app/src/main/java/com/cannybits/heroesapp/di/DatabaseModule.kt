bd750f4b2fc012b49a2a59a23720c183d78350f8
Nyandago <paulmutator@gmail.com>
2022-09-30 18:42:07 +0300

    DatabaseModule.kt

---
+++ app/src/main/java/com/cannybits/heroesapp/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.cannybits.heroesapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.cannybits.heroesapp.data.local.CannyvilleDatabase
+import com.cannybits.heroesapp.util.Constants.CANNYVILLE_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        CannyvilleDatabase::class.java,
+        CANNYVILLE_DATABASE
+    ).build()
+
+}
