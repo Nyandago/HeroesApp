decba5212386f88168693a7a2065b975ace305ce
Nyandago <paulmutator@gmail.com>
2022-09-30 12:08:08 +0300

    CannyvilleDatabase.kt

--- app/src/main/java/com/cannybits/heroesapp/data/local/CannyvilleDatabase.kt
+++ app/src/main/java/com/cannybits/heroesapp/data/local/CannyvilleDatabase.kt
@@ -2,10 +2,16 @@ package com.cannybits.heroesapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.cannybits.heroesapp.data.local.dao.HeroDao
 import com.cannybits.heroesapp.domain.model.Hero
+import com.cannybits.heroesapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class CannyvilleDatabase:RoomDatabase() {
+
     abstract fun heroDao(): HeroDao
+
+    abstract fun heroRemoteKeyDao(): HeroRemoteKey
 }
