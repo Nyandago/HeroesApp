a10c1365cfa9e813797a426472a0ccb814c723df
Nyandago <paulmutator@gmail.com>
2022-10-01 19:27:03 +0300

    HeroRemoteKeyDao.kt

--- app/src/main/java/com/cannybits/heroesapp/data/local/dao/HeroRemoteKeyDao.kt
+++ app/src/main/java/com/cannybits/heroesapp/data/local/dao/HeroRemoteKeyDao.kt
@@ -1,6 +1,8 @@
 package com.cannybits.heroesapp.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.cannybits.heroesapp.domain.model.HeroRemoteKey
 
@@ -10,6 +12,7 @@ interface HeroRemoteKeyDao {
     @Query("SELECT * FROM hero_remote_key_table WHERE id = :id")
     suspend fun getRemoteKey(id: Int): HeroRemoteKey?
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addAllRemoteKeys(heroRemoteKeys: List<HeroRemoteKey>)
 
     @Query("DELETE FROM hero_remote_key_table")
