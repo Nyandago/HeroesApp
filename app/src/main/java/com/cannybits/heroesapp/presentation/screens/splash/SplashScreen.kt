760999d026940b34ef34746c5b386d8a1860157d
Nyandago <paulmutator@gmail.com>
2022-10-03 16:45:36 +0300

    SplashScreen.kt

--- app/src/main/java/com/cannybits/heroesapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/cannybits/heroesapp/presentation/screens/splash/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
@@ -27,10 +28,11 @@ Splash()
 fun Splash(){
     Box(modifier = Modifier
         .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
-        .fillMaxSize()
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ){
         Image(painter = painterResource(id = R.drawable.ic_logo),
-            contentDescription = stringResource(R.string.app_logo)
+            contentDescription = stringResource(R.string.app_logo),
                     )
     }
 
