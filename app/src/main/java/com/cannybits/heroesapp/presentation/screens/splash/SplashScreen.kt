c064259b3e43c50b425b61d030f87ba81e4d01a2
Nyandago <paulmutator@gmail.com>
2022-10-05 12:36:39 +0300

    SplashScreen.kt

--- app/src/main/java/com/cannybits/heroesapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/cannybits/heroesapp/presentation/screens/splash/SplashScreen.kt
@@ -1,16 +1,20 @@
 package com.cannybits.heroesapp.presentation.screens.splash
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.UiMode
 import androidx.navigation.NavHostController
 import com.cannybits.heroesapp.R
 import com.cannybits.heroesapp.ui.theme.Purple500
@@ -26,15 +30,31 @@ Splash()
 
 @Composable
 fun Splash(){
-    Box(modifier = Modifier
-        .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
-        .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Image(painter = painterResource(id = R.drawable.ic_logo),
-            contentDescription = stringResource(R.string.app_logo),
-                    )
+
+    if(isSystemInDarkTheme()){
+        Box(modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo),
+            )
+        }
+
+    }else{
+        Box(modifier = Modifier
+            .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo),
+            )
+        }
+
     }
+
 
 }
 
@@ -42,4 +62,10 @@ fun Splash(){
 @Composable
 fun SplashScreenPreview() {
  Splash()
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun SplashScreenDarkPreview() {
+    Splash()
 }
