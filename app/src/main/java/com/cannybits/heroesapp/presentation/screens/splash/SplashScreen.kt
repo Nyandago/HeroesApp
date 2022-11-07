07d5e088375da9d53d4ad4f8a5595c43593bb9ee
Nyandago <paulmutator@gmail.com>
2022-11-07 18:06:15 +0300

    SplashScreen.kt

--- app/src/main/java/com/cannybits/heroesapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/cannybits/heroesapp/presentation/screens/splash/SplashScreen.kt
@@ -1,20 +1,24 @@
 package com.cannybits.heroesapp.presentation.screens.splash
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.UiMode
 import androidx.navigation.NavHostController
 import com.cannybits.heroesapp.R
 import com.cannybits.heroesapp.ui.theme.Purple500
@@ -23,13 +27,26 @@ import com.cannybits.heroesapp.ui.theme.Purple700
 @Composable
 fun SplashScreen(navController: NavHostController){
 
-Splash()
+    val degrees = remember { Animatable(0f) }
+    
+    LaunchedEffect(key1 = true ){
+        degrees.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+
+            )
+    }
+
+Splash(degrees = degrees.value)
 
 }
 
 
 @Composable
-fun Splash(){
+fun Splash(degrees: Float){
 
     if(isSystemInDarkTheme()){
         Box(modifier = Modifier
@@ -37,7 +54,9 @@ fun Splash(){
             .fillMaxSize(),
             contentAlignment = Alignment.Center
         ){
-            Image(painter = painterResource(id = R.drawable.ic_logo),
+            Image(
+                modifier =Modifier.rotate(degrees = degrees),
+                painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo),
             )
         }
@@ -48,7 +67,9 @@ fun Splash(){
             .fillMaxSize(),
             contentAlignment = Alignment.Center
         ){
-            Image(painter = painterResource(id = R.drawable.ic_logo),
+            Image(
+                modifier = Modifier.rotate(degrees = degrees),
+                painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo),
             )
         }
@@ -61,11 +82,11 @@ fun Splash(){
 @Preview
 @Composable
 fun SplashScreenPreview() {
- Splash()
+ Splash(degrees = 0f)
 }
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun SplashScreenDarkPreview() {
-    Splash()
+    Splash(degrees = 0f)
 }
